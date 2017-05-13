ebb8f35d1f503edf478584ba757de2fb1a06a844
Nikita Boyarskikh <boyanik@yandex-team.ru>
2017-05-13 16:07:55 +0300

    Исправил config.py

--- ask_boyarskikh/config.py
+++ ask_boyarskikh/config.py
@@ -1,7 +1,9 @@
 from configparser import ConfigParser, NoSectionError, NoOptionError
 
 class Configuration:
-	def config_get(self, section, option, default=None):
+	def __init__(self):
 		self.config = ConfigParser()
 		self.config.read("../conf/config.ini")
+
+	def config_get(self, section, option, default=None):
 		return self.config.get(section, option)
